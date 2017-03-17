6c185e5a8e36e61a512ea925f4cb9df1c37baaf6
hoobtron <hoobtron@icloud.com>
2017-03-17 14:58:15 +0100

    main

--- AS2_Lagerhaltung/Program.cs
+++ AS2_Lagerhaltung/Program.cs
@@ -10,6 +10,23 @@ namespace AS2_Lagerhaltung
     {
         static void Main(string[] args)
         {
+            // Arrays erzeugen (mit [n] items)
+            var lieferanten = new Lieferant[2];
+            var lieferkonditionen = new Lieferkondition[4];
+            
+            // Arrays befüllen
+            for (var i = 0; i < lieferanten.Length; i++)
+                lieferanten[i] = new Lieferant();
+            for (var i = 0; i < lieferkonditionen.Length; i++)
+                lieferkonditionen[i] = new Lieferkondition();
+            
+            // ...
+            lieferanten[0].LieferkonditionHinzufuegen(lieferkonditionen[0]);
+            lieferanten[1].LieferkonditionHinzufuegen(lieferkonditionen[2]);
+            lieferanten[1].LieferkonditionHinzufuegen(lieferkonditionen[4]);
+            lieferanten[0].LieferkonditionenEntfernen();
+
+
             //Komposition
             Bestellung bestellung = new Bestellung();
             bestellung.NeueBestellposition();
