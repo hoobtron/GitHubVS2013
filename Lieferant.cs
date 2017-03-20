d369a1fc64ef5a0e2f9a27cab9e991381c6d29be
hoobtron <hoobtron@icloud.com>
2017-03-20 23:50:44 +0100

    Add files via upload

---
+++ Lieferant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2_Lagerhaltung
+{
+    class Lieferant
+    {
+        private string name;
+        private string adresse;
+        private string tel_nr;
+        public string Name { get; set; }
+        public string Adresse { get; set; }
+        public string Tel_nr { get; set; }
+
+        private List<Lieferkondition> _lieferkonditionen = new List<Lieferkondition>();
+
+        public void LieferkonditionHinzufuegen(Lieferkondition lieferkondition)
+        {
+            _lieferkonditionen.Add(lieferkondition);
+        }
+
+        public void LieferkonditionenEntfernen()
+        {
+            foreach (var lieferkondition in _lieferkonditionen)
+                _lieferkonditionen.Remove(lieferkondition);
+        }
+    }
+}
